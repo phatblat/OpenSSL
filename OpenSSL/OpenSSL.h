2c0f436374671542f9830bab31378346bfb6cc0a
Ben Chatelain <ben@octop.ad>
2015-09-04 21:26:27 -0600

    Add project

---
+++ OpenSSL/OpenSSL.h
@@ -0,0 +1,19 @@
+//
+//  OpenSSL.h
+//  OpenSSL
+//
+//  Created by Ben Chatelain on 9/4/15.
+//  Copyright © 2015 Ben Chatelain. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for OpenSSL.
+FOUNDATION_EXPORT double OpenSSLVersionNumber;
+
+//! Project version string for OpenSSL.
+FOUNDATION_EXPORT const unsigned char OpenSSLVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OpenSSL/PublicHeader.h>
+
+
